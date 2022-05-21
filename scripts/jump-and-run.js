270eaf4013285bf5275f51ced6115e9450a49d89
BlackyAxe <71004942+BlackyAxe@users.noreply.github.com>
2022-05-21 13:27:17 +0200

    Sprint 1 - BLI: Spielseite erstellen & Button zum Start Menü Spieleseite in Form einer .html-Webseite. Button noch ohne Verknüpfung zur Start-Seite.

---
+++ scripts/jump-and-run.js
@@ -0,0 +1,9 @@
+function draw() {
+    var canvas = document.getElementById('jump-and-run');
+    var context = canvas.getContext('2d');
+
+    context.canvas.height = window.innerHeight;
+    context.canvas.width = window.innerWidth;
+
+}
+
