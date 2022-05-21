e1c1653651aafdef2890be2f040e1cde21590e1b
Dennis <71004942+BlackyAxe@users.noreply.github.com>
2022-05-21 15:00:30 +0200

    BLI: Phaser (Rendering) einbinden
    Rendering Engine wird überarbeitet.
    WebGL soll über Canvas genutzt werden, sofern möglich.

--- scripts/jump-and-run.js
+++
@@ -1,9 +0,0 @@
-function draw() {
-    var canvas = document.getElementById('jump-and-run');
-    var context = canvas.getContext('2d');
-
-    context.canvas.height = window.innerHeight;
-    context.canvas.width = window.innerWidth;
-
-}
-
