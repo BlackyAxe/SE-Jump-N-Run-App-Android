81518279de2c67874426ebcd9c3299ed62948509
Dennis <71004942+BlackyAxe@users.noreply.github.com>
2022-06-11 12:10:30 +0200

    Sprint 2 - BLI: Charakter erstellen Charakter wurde hinzugefügt. Hintergrundfarbe wurde angepasst und Gravitation wurde hinzugefügt.

---
+++ js/game.js
@@ -0,0 +1,34 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    backgroundColor: '#4488aa',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 2000 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+var game = new Phaser.Game(config);
+var player;
+
+function preload() {
+    this.load.spritesheet('character', 'img/character.png', { frameWidth: 64, frameHeight: 90 });
+}
+
+function create() {
+    player = this.physics.add.sprite(960, 540, 'character');
+
+    player.setBounce(0.1);
+    player.setCollideWorldBounds(true);
+}
+
+function update() {
+}
