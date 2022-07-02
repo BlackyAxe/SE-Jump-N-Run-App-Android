45b49077bb59d17bd15310ed043d18367d1e590e
BlackyAxe <71004942+BlackyAxe@users.noreply.github.com>
2022-07-02 12:57:14 +0200

    Sprint 4: Vorbereitung für Tests

--- js/game/game.js
+++ js/game/game.js
@@ -1,7 +1,6 @@
 var widthGame = window.innerWidth * window.devicePixelRatio;
 var heightGame = window.innerHeight * window.devicePixelRatio;
 var widthBackgroundPicture = 1920
-var heightBackgroundPicture = 1080
 var config = {
     type: Phaser.AUTO,
     width: widthGame,
@@ -22,11 +21,24 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-var map;
 var player;
 var cursors;
+var setBoundsMultiplier = 1
 
-var setBoundsMultiplier = 2
+function playerMovement(player, cursors) {
+    if (cursors.left.isDown)
+        player.setVelocityX(-widthGame / 4);
+
+    else if (cursors.right.isDown)
+        player.setVelocityX(widthGame / 4);
+
+    else
+        player.setVelocityX(0);
+
+    if (this.cursors.up.isDown)
+        this.player.setVelocityY(-heightGame);
+}
+
 
 function preload() {
     this.load.image('background', 'img/background.jpg');
@@ -34,36 +46,25 @@ function preload() {
 }
 
 function create() {
-    this.add.image(0, 0, 'background').setOrigin(0, 0);
-    for (let i = 1; i <= (2^16); i++)
+    for (let i = 0; i <= 16384; i++)
         this.add.image(widthBackgroundPicture * i, 0, 'background').setOrigin(0, 0);
     player = this.physics.add.sprite(widthGame / 2, heightGame / 2, 'character');
-
     player.setBounce(0.5);
     player.setCollideWorldBounds(true);
-
     this.cameras.main.startFollow(player, true, 0.05, 0.05);
-
     cursors = this.input.keyboard.createCursorKeys();
-    text = this.add.text(10, 60, 'Cursors to move', { font: '16px Courier', fill: '#00ff00' }).setScrollFactor(0);
+    coordinatesOverlay = this.add.text(10, 60, 'Cursors to move', { font: '16px Courier', fill: '#00ff00' }).setScrollFactor(0);
 }
 
 function update() {
+    setBoundsMultiplier = setBoundsMultiplier + 1
     this.cameras.main.setBounds(0, 0, widthGame * setBoundsMultiplier, heightGame);
     this.physics.world.setBounds(0, 0, widthGame * setBoundsMultiplier, heightGame);
+    
 
-    if (cursors.left.isDown)
-        player.setVelocityX(-widthGame / 4);
-    else if (cursors.right.isDown) {
-        setBoundsMultiplier = setBoundsMultiplier + 1
-        player.setVelocityX(widthGame / 4);
-    }
-    else
-        player.setVelocityX(0);
-    if (cursors.up.isDown)
-        player.setVelocityY(-heightGame);
+    playerMovement(player, cursors);
 
-    text.setText([
+    coordinatesOverlay.setText([
         'mouse x: ' + this.input.x,
         'mouse y: ' + this.input.y,
         'player x: ' + player.body.position.x,
